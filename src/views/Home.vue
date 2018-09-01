2be525e9c2f9d08118b53e0b12d9b965cd536a14
Rodolphe Stoclin <srodolphe@gmail.com>
2018-09-01 21:12:09 +0200

    Some cleaning

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     <div v-for="item in listItems" :key="item.id">
       <router-link :to="'/item/' + item.id">{{ item.title }}</router-link>
     </div>
@@ -9,14 +7,9 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  },
+  components: {},
   created: function() {
     this.$store.dispatch("GET_LIST_ITEMS");
   },
